bc7fcfa55a36f153270d0203d21e1be7a9fde9f9
Dina Riella <madishfg@gmail.com>
2025-02-24 22:00:33 +0300

    first commit

---
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -0,0 +1,18 @@
+package jm.task.core.jdbc;
+
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+public class Main {
+    public static void main(String[] args) {
+        // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Bob", "Black", (byte) 45);
+        userService.saveUser("Alice", "Black", (byte) 45);
+        userService.saveUser("Bobbie", "Black", (byte) 5);
+        userService.removeUserById(1);
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
+    }
+}
